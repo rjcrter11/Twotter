cc0c91f5f7362635ecfe13778f91b75f8fa9bb49
Rachel <rjcrter11@gmail.com>
2020-10-20 09:56:05 -0500

    moves user profile logic into its own component

---
+++ twotter/src/components/UserProfile.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile_user-panel">
+      <h1 class="user-profile_username">@{{ user.username }}</h1>
+      <div class="user-profile_follower-count">
+        <strong> Followers: </strong> {{ followers }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_rjcrter11",
+        firstName: "Rachel",
+        lastName: "Carter",
+        email: "rjcrter11@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower`);
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName} `;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+
+.user-profile_user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+h1 {
+  margin: 0;
+}
+</style>
